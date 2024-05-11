a4910a9b6e9dede8c7564d6763aa66837e448cfd
rajabmohammadi <rajab.mohammadi222@gmail.com>
2024-05-11 08:07:43 +0430

    add rajab mohammadi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rajab Mohammadi
         </p>
         <a
           className="App-link"
